ab53b48e5154ab4538ae85460565b2cc87b22472
Caleb Castro <ccastrop@csticorp.biz>
2023-03-28 10:39:06 -0500

    update: regex para validacion y estilos de la inscripcion

--- app/src/app/components/dialog-inscription/dialog-inscription.component.ts
+++ app/src/app/components/dialog-inscription/dialog-inscription.component.ts
@@ -1,94 +1,113 @@
 import { Component, OnInit, Inject, ViewChild } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormArray, Validators, FormControl } from "@angular/forms";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import {
+  FormBuilder,
+  FormArray,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { GeneralDialogComponent } from '../../components/general-dialog/general-dialog.component';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-inscription',
   templateUrl: './dialog-inscription.component.html',
-  styleUrls: ['./dialog-inscription.component.scss']
+  styleUrls: ['./dialog-inscription.component.scss'],
 })
 export class DialogInscriptionComponent implements OnInit {
   @ViewChild('country') country: HTMLInputElement | undefined;
-  buttonName = "Editar integrante"
-  selected2:any
+  buttonName = 'Añadir integrante';
+  selected2: any;
   optionsCountry: any[] = [];
   filteredOptionsCountry: any;
   countryCtrl = new FormControl();
-  universityCards: any[] = []
+  universityCards: any[] = [];
   displayedColumns: string[] = ['position', 'weight', 'symbol', 'delete'];
   participants: any;
   toggleDisabled = false;
-  isoCountry: string | undefined
-  other= false
-  spinner= false;
+  isoCountry: string | undefined;
+  other = false;
+  spinner = false;
   myControl = new FormControl();
-  constructor(@ Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<DialogInscriptionComponent>, public dialog: MatDialog, public fb: FormBuilder, private router: Router) {
-
-   }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<DialogInscriptionComponent>,
+    public dialog: MatDialog,
+    public fb: FormBuilder,
+    private router: Router
+  ) {}
   registrationForm = this.fb.group({
-    nombreCompleto: ["", Validators.required],
-    carrera: ["", Validators.required],
-    email: ["",Validators.email],
-    nombreCoordinador: [""],
-    correoCoordinador: ["", Validators.email],
-    linkedin: "",
-    teamLeader: ["", Validators.required],
+    nombreCompleto: ['', [Validators.pattern(/^[a-zA-ZÀ-ÿ\s]{5,40}$/), Validators.required]],
+    carrera: ['', [Validators.pattern(/^[a-zA-ZÀ-ÿ\s]{5,40}$/), Validators.required]],
+    email: ['', [Validators.pattern(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/), Validators.required]],
+    nombreCoordinador: ['', [Validators.pattern(/^[a-zA-ZÀ-ÿ\s]{5,40}$/), Validators.required]],
+    correoCoordinador: ['', [Validators.pattern(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/), Validators.required]],
+    linkedin: ['', Validators.pattern(/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)\/([-a-zA-Z0-9]+)\/*/gm)],
+    teamLeader: ['', Validators.required],
     confirmation: true,
-    index: ""
-  })
+    index: '',
+  });
 
   ngOnInit(): void {
-    console.log(this.data)
-    if(this.data.mode== "edit"){
-      this.buttonName= "Editar integrante"
-        this.registrationForm.patchValue({
-          teamLeader: this.data.dataSource[this.data.index].teamLeader,
-          nombreCompleto: this.data.dataSource[this.data.index].nombreCompleto,
-          carrera: this.data.dataSource[this.data.index].carrera,
-          email: this.data.dataSource[this.data.index].email,
-          nombreCoordinador: this.data.dataSource[this.data.index].nombreCoordinador,
-          correoCoordinador: this.data.dataSource[this.data.index].correoCoordinador,
-          linkedin: this.data.dataSource[this.data.index].linkedin,
-          confirmation: true,
-          index: this.data.dataSource[this.data.index].index
-        })
-          this.registrationForm.addControl('nameOther',new FormControl('', Validators.required))
-          this.registrationForm.patchValue({
-            nameOther:this.data.dataSource[this.data.index].nameOther,
-          })
-        }
-
-      }
-
+    console.log(this.data);
+    if (this.data.mode == 'edit') {
+      this.buttonName = 'Editar integrante';
+      this.registrationForm.patchValue({
+        teamLeader: this.data.dataSource[this.data.index].teamLeader,
+        nombreCompleto: this.data.dataSource[this.data.index].nombreCompleto,
+        carrera: this.data.dataSource[this.data.index].carrera,
+        email: this.data.dataSource[this.data.index].email,
+        nombreCoordinador:
+          this.data.dataSource[this.data.index].nombreCoordinador,
+        correoCoordinador:
+          this.data.dataSource[this.data.index].correoCoordinador,
+        linkedin: this.data.dataSource[this.data.index].linkedin,
+        confirmation: true,
+        index: this.data.dataSource[this.data.index].index,
+      });
+      this.registrationForm.addControl(
+        'nameOther',
+        new FormControl('', Validators.required)
+      );
+      this.registrationForm.patchValue({
+        nameOther: this.data.dataSource[this.data.index].nameOther,
+      });
+    }
+  }
 
   getAutocompleteName(item: any) {
     return item.name;
   }
 
-  namenameOther(value: any){
-    if(value.value == 1){
+  namenameOther(value: any) {
+    if (value.value == 1) {
       this.other = true;
-      this.registrationForm.addControl('nameOther',new FormControl('', Validators.required))
-    }else{
-      this.other = false
-      this.registrationForm.removeControl('nameOther')
+      this.registrationForm.addControl(
+        'nameOther',
+        new FormControl('', Validators.required)
+      );
+    } else {
+      this.other = false;
+      this.registrationForm.removeControl('nameOther');
     }
   }
-  close(){
-    this.dialogRef.close()
+  close() {
+    this.dialogRef.close();
   }
   addParticipant() {
-    console.log(this.registrationForm.value)
+    console.log(this.registrationForm.value);
     if (this.registrationForm.valid) {
-      this.dialogRef.close(this.registrationForm.value)
+      this.dialogRef.close(this.registrationForm.value);
     } else {
       this.dialog.open(GeneralDialogComponent, {
         data: {
-          message: "Complete de manera correcta la información de inscripción"
-        }
-      })
+          message: 'Complete de manera correcta la información de inscripción',
+        },
+      });
     }
   }
 }
